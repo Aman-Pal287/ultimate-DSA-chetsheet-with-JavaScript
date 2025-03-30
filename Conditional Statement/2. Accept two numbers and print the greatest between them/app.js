2643bc44fec7b5a371765490eb47e41f4672de5b
Aman-Pal287 <palaman9786@gmail.com>
2025-03-31 00:01:08 +0530

    Accept two numbers and print the greatest between them

--- Conditional Statement/2. Accept two numbers and print the greatest between them/app.js
+++ Conditional Statement/2. Accept two numbers and print the greatest between them/app.js
@@ -8,4 +8,6 @@ let num2 = prompt("Enter num2")
 if(num1>num2) console.log("num1 is the greatest");
 else console.log("num2 is the greatest");
 
+//time complexity - O(1)
+//space complexity - O(1)
 
