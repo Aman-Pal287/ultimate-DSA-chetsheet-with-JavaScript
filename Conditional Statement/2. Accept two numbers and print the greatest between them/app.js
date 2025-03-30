b27c38eba5d9aae5d5cad93972b34374eea8b5e6
Aman-Pal287 <palaman9786@gmail.com>
2025-03-30 23:59:03 +0530

    Accept two numbers and print the greatest between them

---
+++ Conditional Statement/2. Accept two numbers and print the greatest between them/app.js
@@ -0,0 +1,11 @@
+// 2. Accept two numbers and print the greatest between them
+
+let prompt = require("prompt-sync")();
+
+let num1 = prompt("Enter num1")
+let num2 = prompt("Enter num2")
+
+if(num1>num2) console.log("num1 is the greatest");
+else console.log("num2 is the greatest");
+
+
