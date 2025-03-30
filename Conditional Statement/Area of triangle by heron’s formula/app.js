66120840b3ef1badd7e975054d5c675b472ecf85
Aman-Pal287 <palaman9786@gmail.com>
2025-03-30 23:41:59 +0530

    Area of triangle by heron’s formula

---
+++ Conditional Statement/Area of triangle by heron’s formula/app.js
@@ -0,0 +1,16 @@
+// // Area of triangle by heron’s formula
+
+// function heronsFormula(a, b, c) {
+//     // Semi-perimeter (s)
+//     let s = (a + b + c) / 2;
+
+//     // Heron's formula लगाओ
+//     let area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+
+//     return area;
+// }
+
+// // Example use case
+// let a = 5, b = 6, c = 7;
+// let area = heronsFormula(a, b, c);
+// console.log("Triangle's Area:", area.toFixed(2)); // 2 decimal places
