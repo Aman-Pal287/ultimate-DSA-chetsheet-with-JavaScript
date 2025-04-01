4a08d915d7bb6f1f420a98be60775557b818afdd
Aman-Pal287 <palaman9786@gmail.com>
2025-04-01 14:37:26 +0530

    Array left rotation by K element

---
+++ Arrays/11.  Array left rotation by K element(Imp)/app.js
@@ -0,0 +1,52 @@
+// problem 1 : Array left rotation by K element
+
+const prompt = require('prompt-sync')();
+
+
+// method 1: brut  force method (gahtiya tarika)
+// let arr = [1, 2, 3, 4, 5]
+// let k = prompt("Enter a number : ")
+
+// for (let j = 1; j <= k; j++) {
+//     let copy = arr[0]
+//     for (let i = 0; i <= arr.length - 1; i++) {
+//         arr[i] = arr[i + 1]
+//     }
+//     arr[arr.length - 1] = copy;
+// }
+// console.log(arr);
+
+// Time Complexity: O(k*n)
+// Space Complexity: O(1)
+
+
+
+// method 2: method 1 ka hi soution hai but k % arr.length karenge taaki unncessary loop naa chale
+// let arr = [1, 2, 3, 4, 5]
+// let k = prompt("enter a number: ")
+// k = k % arr.length;
+// for (let j = 1;j<=k; j++) {
+//     let copy = arr[0]
+//     for (let i = 0; i < arr.length - 1; i++) {
+//         arr[i] = arr[i + 1]
+//     }
+//     arr[arr.length - 1] = copy
+// }
+// console.log(arr);
+
+// Time Complexity: O(k*n)
+// Space Complexity: O(1)
+
+
+// method 3 : efficient tarika
+let arr = [1,2,3,4,5]
+let temp = new Array(arr.length);
+let k = prompt('enter a number : ')
+k = k % arr.length;
+for (let i=0 ; i<arr.length;i++){
+    temp[i] = arr[(i+k)%arr.length]
+}
+console.log(temp);
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
