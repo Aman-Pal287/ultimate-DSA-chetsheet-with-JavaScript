acc9f819b3e2372dc02875ce9e3730592e66954e
Aman-Pal287 <palaman9786@gmail.com>
2025-04-19 15:05:11 +0530

    // leetcode : two sum

--- Map/3. leetcode  - two sum/app.js
+++ Map/3. leetcode  - two sum/app.js
@@ -14,3 +14,19 @@
 //     }
 // }
 // console.log(ans);
+
+
+//efficient method
+let num = [11,2,15,7]
+let target = 9;
+let map = new Map();
+let ans = [-1,-1];
+for(let i=0;i<num.length;i++){
+    if(map.has(target - num[i])){
+        ans[1] = i;
+        ans[0] = map.get(target - num[i])
+        break;
+    }
+    else map.set(num[i],i)
+}
+console.log(ans);
