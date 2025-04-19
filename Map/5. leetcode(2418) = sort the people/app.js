76af2ec07d7c05ba61a3666d4b912cacfe86c185
Aman-Pal287 <palaman9786@gmail.com>
2025-04-19 15:17:55 +0530

    // leetcode(2418): sort the people
    //method 1 : using array of object

---
+++ Map/5. leetcode(2418) = sort the people/app.js
@@ -0,0 +1,18 @@
+// leetcode(2418): sort the people 
+
+//method 1 : using array of object
+// let names = ["Mary", "John", "Emma"];
+// let heights = [180, 165, 170];
+
+// let people = []
+// for(let i=0;i<names.length;i++){
+//     people.push({name:names[i],height:heights[i]})
+// }
+// people.sort((a,b) => b.height - a.height);
+
+// let ans = new Array(people.length)
+// for(let i=0;i<ans.length;i++){
+//     ans[i] = people[i].name;
+// }
+// console.log(ans);
+
