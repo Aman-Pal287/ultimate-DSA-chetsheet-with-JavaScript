ce3998103a050c109bb52190573ec3806de6b4bd
Aman-Pal287 <palaman9786@gmail.com>
2025-05-01 01:16:41 +0530

    problem 3: sum of upto n terms

---
+++ Recursion And Memory/live class/6. sum of upto n terms/app.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+
+
+//^ problem 3: sum of upto n terms
+function temp(n){
+    if(n == 1) return n;
+    return n + temp(n-1)
+}
+let n = Number(prompt("enter a number:  "))
+console.log(temp(n))
