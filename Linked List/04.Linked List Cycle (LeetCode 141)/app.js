b985d78975f0ce859728944caf76a8c2989c8bc6
Aman-Pal287 <palaman9786@gmail.com>
2025-06-17 15:11:05 +0530

    /! Linked List Cycle (LeetCode 141)

--- Linked List/04.Linked List Cycle (LeetCode 141)/app.js
+++ Linked List/04.Linked List Cycle (LeetCode 141)/app.js
@@ -11,3 +11,36 @@ function linkedListCycle(head) {
     }
 
 }
+
+
+//! Linked List Cycle 2 (LeetCode 142)
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function (head) {
+    if (head == null || head.next == null) return null
+
+    let fast = head, slow = head
+    while (fast != null && fast.next != null) {
+        fast = fast.next.next
+        slow = slow.next
+        if (fast == slow) {
+            let entry = head;
+            while (entry !== slow) {
+                entry = entry.next;
+                slow = slow.next;
+            }
+            return entry;
+        }
+    }
+    return null
+};
